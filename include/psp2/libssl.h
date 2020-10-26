d48b04d8586f96f50c6f8a62b8dcb7d91d324491
cuevavirus <53134627+cuevavirus@users.noreply.github.com>
2020-10-25 20:22:25 -0400

    Refactor psp2/libssl.h

--- include/psp2/libssl.h
+++ include/psp2/libssl.h
@@ -1,54 +1,55 @@
-/**
- * \usergroup{SceLibSsl}
- * \usage{psp2/libssl.h,SceSsl_stub}
- */
+#ifndef _DOLCESDK_PSP2_LIBSSL_H_
+#define _DOLCESDK_PSP2_LIBSSL_H_
 
-
-#ifndef _PSP2_SSL_LIBSSL_H
-#define _PSP2_SSL_LIBSSL_H
-
+#include <psp2/types.h>
 #include <psp2/rtc.h>
 
-typedef enum SceSslErrorCode {
-	SCE_SSL_ERROR_BEFORE_INIT    = (int)0x80435001,
-	SCE_SSL_ERROR_ALREADY_INITED = (int)0x80435020,
-	SCE_SSL_ERROR_OUT_OF_MEMORY  = (int)0x80435022,
-	SCE_SSL_ERROR_NOT_FOUND      = (int)0x80435025,
-	SCE_SSL_ERROR_INTERNAL       = (int)0x80435026,
-	SCE_SSL_ERROR_INVALID_FORMAT = (int)0x80435108,
-	SCE_SSL_ERROR_INVALID_VALUE  = (int)0x804351FE
-} SceSslErrorCode;
+
+#define SCE_SSL_ERROR_BEFORE_INIT		-2143072255	/*(0x80435001)	*/
+#define SCE_SSL_ERROR_ALREADY_INITED	-2143072224	/*(0x80435020)	*/
+#define SCE_SSL_ERROR_OUT_OF_MEMORY		-2143072222	/*(0x80435022)	*/
+#define SCE_SSL_ERROR_INTERNAL			-2143072218	/*(0x80435026)	*/
+#define SCE_SSL_ERROR_NOT_FOUND			-2143072219	/*(0x80435025)	*/
+#define SCE_SSL_ERROR_INVALID_VALUE		-2143071746	/*(0x804351FE)	*/
+#define SCE_SSL_ERROR_INVALID_FORMAT	-2143071992	/*(0x80435108)	*/
+#define SCE_SSL_ERROR_R_ERROR_BASE		-2143071744 /*(0x80435200)	*/
+#define SCE_SSL_ERROR_R_ERROR_FAILED	-2143071743 /*(0x80435201)	*/
+#define SCE_SSL_ERROR_R_ERROR_UNKNOWN	-2143071489	/*(0x804352FF)	*/
 
 typedef void SceSslCert;
 typedef void SceSslCertName;
 
 typedef struct SceSslMemoryPoolStats{
-	unsigned poolSize;
-	unsigned maxInuseSize;
-	unsigned currentInuseSize;
-	int  reserved;
+	SceSize		poolSize;
+	SceSize		maxInuseSize;
+	SceSize		currentInuseSize;
+	SceInt32	reserved;
 } SceSslMemoryPoolStats;
 
-#ifdef __cplusplus
+#if defined(_LANGUAGE_C_PLUS_PLUS)||defined(__cplusplus)||defined(c_plusplus)
 extern "C" {
-#endif
+#endif	/* defined(_LANGUAGE_C_PLUS_PLUS)||defined(__cplusplus)||defined(c_plusplus) */
 
+/*E define for compatibility*/
+/*J PSPとの互換用の定義 */
 #define sceSslEnd()	sceSslTerm()
 
-int sceSslInit(unsigned poolSize);
-int sceSslTerm(void);
-int sceSslGetMemoryPoolStats(SceSslMemoryPoolStats* currentStat);
-int sceSslGetSerialNumber(SceSslCert *sslCert, const char **sboData, unsigned *sboLen);
-int sceSslGetNotBefore(SceSslCert *sslCert, SceRtcTick *begin);
-int sceSslGetNotAfter(SceSslCert *sslCert, SceRtcTick *limit);
-int sceSslGetNameEntryCount(SceSslCertName *certName);
-int sceSslGetNameEntryInfo(SceSslCertName *certName, int entryNum, char *oidname, unsigned maxOidnameLen, char *value, unsigned maxValueLen, unsigned *valueLen);
+SceInt32 sceSslInit(SceSize poolSize);
+SceInt32 sceSslTerm(void);
+SceInt32 sceSslGetMemoryPoolStats(SceSslMemoryPoolStats* currentStat);
+
+SceInt32 sceSslGetSerialNumber(SceSslCert *sslCert, const SceUChar8 **sboData, SceSize *sboLen);
 SceSslCertName *sceSslGetSubjectName(SceSslCert *sslCert);
 SceSslCertName *sceSslGetIssuerName (SceSslCert *sslCert);
-int sceSslFreeSslCertName(SceSslCertName* certName);
+SceInt32 sceSslGetNotBefore(SceSslCert *sslCert, SceRtcTick *begin);
+SceInt32 sceSslGetNotAfter(SceSslCert *sslCert, SceRtcTick *limit);
+SceInt32 sceSslGetNameEntryCount(SceSslCertName *certName);
+SceInt32 sceSslGetNameEntryInfo(SceSslCertName *certName, SceInt32 entryNum, SceChar8 *oidname, SceSize maxOidnameLen, SceUChar8 *value, SceSize maxValueLen, SceSize *valueLen);
+SceInt32 sceSslFreeSslCertName(SceSslCertName* certName);
 
-#ifdef __cplusplus
+#if defined(_LANGUAGE_C_PLUS_PLUS)||defined(__cplusplus)||defined(c_plusplus)
 }
-#endif
-#endif
+#endif	/* defined(_LANGUAGE_C_PLUS_PLUS)||defined(__cplusplus)||defined(c_plusplus) */
+
+#endif	/* _DOLCESDK_PSP2_LIBSSL_H_ */
 
