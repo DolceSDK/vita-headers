35cb0b73f9d495c0aeb1ff1c32f3da5bc3da1963
cuevavirus <53134627+cuevavirus@users.noreply.github.com>
2020-10-26 00:22:10 -0400

    Refactor Travis CI scripts

--- .travis.d/last_built_toolchain.py
+++ .travis.d/last_built_toolchain.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+# This file is for backward compatibility with older ddpm
+
 import json
 import urllib.request as request
 
