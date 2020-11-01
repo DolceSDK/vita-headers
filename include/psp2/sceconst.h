d37ca295f8332af801a0ea64d090674b430c56c6
cuevavirus <53134627+cuevavirus@users.noreply.github.com>
2020-11-01 14:36:17 -0500

    Add sceconst.h

---
+++ include/psp2/sceconst.h
@@ -0,0 +1,7 @@
+#ifndef _DOLCESDK_PSP2_SCECONST_H_
+#define _DOLCESDK_PSP2_SCECONST_H_
+
+#include <psp2common/sceconst.h>
+
+#endif  /* _DOLCESDK_PSP2_SCECONST_H_ */
+
---
+++ include/psp2common/sceconst.h
@@ -0,0 +1,29 @@
+#ifndef _DOLCESDK_PSP2COMMON_SCECONST_H_
+#define _DOLCESDK_PSP2COMMON_SCECONST_H_
+
+#define SCE_MATH_E			2.7182818284590452354		/**< e            */
+#define SCE_MATH_LOG2E		1.4426950408889634074		/**< log2(e)      */
+#define SCE_MATH_LOG10E		0.43429448190325182765		/**< log10(e)     */
+#define SCE_MATH_LN2		0.69314718055994530942		/**< ln(2)        */
+#define SCE_MATH_LN10		2.30258509299404568402		/**< ln(10)       */
+#define SCE_MATH_PI			3.14159265358979323846		/**< pi           */
+#define SCE_MATH_TWOPI		(SCE_MATH_PI * 2.0)			/**< 2 * pi       */
+#define SCE_MATH_PI_2		1.57079632679489661923		/**< pi / 2       */
+#define SCE_MATH_PI_4		0.78539816339744830962		/**< pi / 4       */
+#define SCE_MATH_3PI_4		2.3561944901923448370E0		/**< 3 * pi / 2   */
+#define SCE_MATH_SQRTPI		1.77245385090551602792981	/**< sqrt(pi)     */
+#define SCE_MATH_1_PI		0.31830988618379067154		/**< 1 / pi       */
+#define SCE_MATH_2_PI		0.63661977236758134308		/**< 2 / pi       */
+#define SCE_MATH_2_SQRTPI	1.12837916709551257390		/**< 2 / sqrt(pi) */
+#define SCE_MATH_SQRT2		1.41421356237309504880		/**< sqrt(2)      */
+#define SCE_MATH_SQRT1_2	0.70710678118654752440		/**< 1 / sqrt(2)  */
+#define SCE_MATH_SQRT3		1.73205080756887729353		/**< sqrt(3)      */
+#define SCE_MATH_IVLN10		0.43429448190325182765		/**< 1 / log(10)  */
+#define SCE_MATH_INVLN2		1.4426950408889633870E0		/**< 1 / log(2)   */
+
+#define SCE_MATH_LOG10TWO	0.30102999566398119521		/**< log10(2)     */
+#define SCE_MATH_LOG2TEN	3.32192809488736234787		/**< log2(10)     */
+#define SCE_MATH_SQRT3_2	0.86602540378443864676		/**< sqrt(3) / 2  */
+
+#endif  /* _DOLCESDK_PSP2COMMON_SCECONST_H_ */
+
