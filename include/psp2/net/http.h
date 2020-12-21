664f7406632c3a93b09f6e517218f5151085a753
cuevavirus <53134627+cuevavirus@users.noreply.github.com>
2020-12-20 19:58:17 -0500

    Remove deprecated net headers

--- include/psp2/net/http.h
+++
@@ -1,9 +0,0 @@
-#ifndef _DOLCESDK_PSP2_NET_HTTP_H_
-#define _DOLCESDK_PSP2_NET_HTTP_H_
-
-#pragma message "<psp2/net/http.h> is deprecated. Please include <psp2/libhttp.h>."
-
-#include <psp2/libhttp.h>
-
-#endif /* _DOLCESDK_PSP2_NET_HTTP_H_ */
-
